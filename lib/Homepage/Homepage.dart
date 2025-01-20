ccf67edfd902879fbf04b03840a79f29d89652e8
GivenChauke <chaukemnashe123@gmail.com>
2025-01-20 15:13:22 +0200

    add homepage with sidemenu and carousel

--- lib/Homepage/Homepage.dart
+++ lib/Homepage/Homepage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:chatbot/components/side_menu.dart';
 
@@ -9,6 +10,25 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final urlImages=[
+    'assets/images/img9.jpg',
+    'assets/images/img6.jpeg',
+    'assets/images/img7.jpg',
+    'assets/images/img2.jpg',
+    'assets/images/img11.jpg'
+  ];
+  Widget buildImage(String urlImage, int index) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: ClipRRect(
+        child: Image.asset(
+          urlImage,
+          fit: BoxFit.contain
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +42,22 @@ class _HomepageState extends State<Homepage> {
           ),
         ),
       ),
-      body:const Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Center(
-          child: const Text("Welcome to the Homepage!"),
+      body:Center(
+        child: CarouselSlider.builder(
+          itemCount: urlImages.length,
+          itemBuilder: (context, index, realIndex) {
+            final urlImage = urlImages[index];
+            return buildImage(urlImage, index);
+          },
+            options:CarouselOptions(
+            height: 400,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 3),
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height
+          ),
         ),
-      ),
+      )
     );
   }
 }
